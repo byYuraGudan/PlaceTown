43ccf0213f5218aba5846d57e441e38074a06cda
byYuraGudan <yura.gudan@gmail.com>
2020-05-16 16:59:50 +0300

    change wsgi to settings

--- PlaceTown/wsgi.py
+++ PlaceTown/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlaceTown.test_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PlaceTown.settings')
 
 application = get_wsgi_application()
