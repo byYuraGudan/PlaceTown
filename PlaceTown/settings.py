b58368e65ae1bf1c618782c2c8485ae8204cf6a6
byYuraGudan <yura.gudan@gmail.com>
2020-06-09 20:52:43 +0300

    add geo loc

--- PlaceTown/settings.py
+++ PlaceTown/settings.py
@@ -163,6 +163,5 @@ DJANGO_TELEGRAMBOT = {
     ],
 }
 
-# # GEO
-# GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
-# GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
